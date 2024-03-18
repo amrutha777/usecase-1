4633a21e51ad2c3ecaf6ef2bf2570e4b9feaae6d
amrutha777 <147131279+amrutha777@users.noreply.github.com>
2024-03-18 17:37:31 +0530

    Update main.tf

--- DbServer/main.tf
+++ DbServer/main.tf
@@ -6,7 +6,6 @@ resource "google_compute_instance" "db-server" {
   name         = var.db-server-name
   machine_type = var.machine-type
   zone         = var.zone
-  //tags         = ["db"]
   boot_disk {
     initialize_params {
       image    = var.machine-image  
